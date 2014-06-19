5b8f2383c685a8b167a314ffb0284b4686b84a6a
waysaku <waysaku@gmail.com>
2014-06-19 10:28:15 +0900

    first commit

---
+++ collect/collectViewController.m
@@ -0,0 +1,29 @@
+//
+//  collectViewController.m
+//  collect
+//
+//  Created by 渡邉 雄作 on 6/19/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "collectViewController.h"
+
+@interface collectViewController ()
+
+@end
+
+@implementation collectViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ collectTests/collectTests.m
@@ -0,0 +1,34 @@
+//
+//  collectTests.m
+//  collectTests
+//
+//  Created by 渡邉 雄作 on 6/19/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface collectTests : XCTestCase
+
+@end
+
+@implementation collectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
