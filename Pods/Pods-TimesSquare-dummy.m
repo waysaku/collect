e1b6f87c3149ebd77d1272eac4b42889a07f003d
waysaku <waysaku@gmail.com>
2014-06-19 18:47:48 +0900

    temporary commit

---
+++ Pods/Pods-TimesSquare-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TimesSquare : NSObject
+@end
+@implementation PodsDummy_Pods_TimesSquare
+@end
---
+++ collect/HogeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HogeViewController.h
+//  collect
+//
+//  Created by 渡邉 雄作 on 6/19/14.
+//  Copyright (c) 2014 渡邉 雄作. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HogeViewController : UIView
+
+@end
